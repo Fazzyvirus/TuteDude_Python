4d06aea2d7fb86ac621a630444c74e06a8847bcc
fazzyvirus <fazzyvirus@gmail.com>
2025-05-03 01:51:36 +0530

    commit1

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+# Task 1: Perform Basic Mathematical Operations
+print('Enter the first number: ')
+a = float(input())
+print('Enter the second number: ')
+b = float(input())
+
+print('Addition: ', a+b)
+print('Subtraction: ', a-b)
+print('Multiplication: ', a*b)
+print('Division: ', a/b)
+
+
+#Task 2: Create a Personalized Greeting
+print('Enter your first name: ')
+fname = input()
+print('Enter your last name: ')
+lname = input()
+
+print('Hello ' + fname + ' ' + lname + '! Welcome to the Python program.')
