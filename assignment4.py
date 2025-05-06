e8b1dfd93eed542d402624f6adda7016d486c199
Fazzyvirus <145501058+Fazzyvirus@users.noreply.github.com>
2025-05-07 01:02:25 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,32 @@
+#Task 1: Read a File and Handle Errors
+# try:
+#     file = open('sample.txt', 'r')
+#     print(file.read())
+#     file.close()
+# except FileNotFoundError:
+#     print("The File sample.txt not found")
+# finally:
+#     print()
+
+#Task 2: Write and Append Data to a File
+try:
+    file = open('output.txt', 'w')
+    userInput = input("Enter some text: ")
+    inserted = file.write(userInput)
+    print('Data successfully written to output.txt')
+    file.close()
+
+    file = open('output.txt', 'a')
+    userInput = input("Enter Additional text: ")
+    append = file.write(userInput)
+    print('Data successfully Appended')
+    file.close()
+
+    print('Final content of output.txt: ')
+    file = open('output.txt', 'r')
+    print(file.read())
+    file.close()
+except FileExistsError:
+    print("The File output.txt not found")
+finally:
+    print()
